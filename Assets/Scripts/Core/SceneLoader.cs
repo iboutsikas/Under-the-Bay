d45540f0a2153d26e86a85bf379dd99528f4f57c
Ioannis Boutsikas <mgioanni@outlook.com>
2021-09-20 13:36:48 -0400

    Fix UI text and positioning

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,57 @@
+using UnityEngine.SceneManagement;
+using static UTB.EventSystem.SceneLoadingEvents;
+
+namespace UTB.Core
+{
+    public class SceneLoader : MonoBehaviourSingletonPersistent<SceneLoader>
+    {
+        private int m_CurrentSceneIndex = -1;
+
+        public SceneConfiguration SceneConfig;
+        public bool LoadFirstSceneOnStart = true;
+
+
+        private void OnEnable()
+        {
+            RequestSceneLoadEvent.Subscribe(On_SceneLoadRequested);
+
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        private void OnDisable()
+        {
+            RequestSceneLoadEvent.Unsubscribe(On_SceneLoadRequested);
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        private void Start()
+        {
+            if (!LoadFirstSceneOnStart || !(SceneConfig.Scenes.Count > 0))
+                return;
+            var scene = SceneConfig.Scenes[0];
+            SceneManager.LoadSceneAsync(scene.BuildIndex, LoadSceneMode.Additive);
+        }
+
+        private void On_SceneLoadRequested(RequestSceneLoadEvent info)
+        {
+            if (m_CurrentSceneIndex != -1)
+                SceneManager.UnloadSceneAsync(m_CurrentSceneIndex);
+
+            LoadSceneMode mode = info.IsAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+
+            SceneManager.LoadSceneAsync(info.SceneIndex, mode);
+        }
+
+        private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            m_CurrentSceneIndex = scene.buildIndex;
+
+            SceneLoadedEvent evt = new SceneLoadedEvent();
+            evt.BuildIndex = m_CurrentSceneIndex;
+
+            evt.Fire();
+        }
+    }
+
+
+}
--- Assets/Scripts/Core/StateManager.cs
+++ Assets/Scripts/Core/StateManager.cs
@@ -1,15 +1,12 @@
 using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using static UTB.EventSystem.SceneLoadingEvents;
+
 using static UTB.EventSystem.DataEvents;
 
 namespace UTB.Core
 {
     public class StateManager : MonoBehaviourSingletonPersistent<StateManager>
     {
-        private int m_CurrentSceneIndex = -1;
-
         public DateTimeOffset? FromDate;
         public DateTimeOffset? ToDate;
 
@@ -21,35 +18,14 @@ namespace UTB.Core
 
         private void OnEnable()
         {
-            RequestSceneLoadEvent.Subscribe(On_SceneLoadRequested);
             SampleDatesChangedEvent.Subscribe(On_SampleDatesChanged);
 
-            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
         }
 
         private void OnDisable()
         {
-            RequestSceneLoadEvent.Unsubscribe(On_SceneLoadRequested);
             SampleDatesChangedEvent.Unsubscribe(On_SampleDatesChanged);
 
-            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
-        }
-
-        
-
-        private void Start()
-        {
-            //SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-        }
-
-        private void On_SceneLoadRequested(RequestSceneLoadEvent info)
-        {
-            if (m_CurrentSceneIndex != -1)
-                SceneManager.UnloadSceneAsync(m_CurrentSceneIndex);
-
-            LoadSceneMode mode = info.IsAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single;
-
-            SceneManager.LoadSceneAsync(info.SceneIndex, mode);
         }
 
         private void On_SampleDatesChanged(SampleDatesChangedEvent info)
@@ -61,14 +37,6 @@ namespace UTB.Core
                 ToDate = info.To.Value;
         }
 
-        private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
-        {
-            m_CurrentSceneIndex = scene.buildIndex;
 
-            SceneLoadedEvent evt = new SceneLoadedEvent();
-            evt.BuildIndex = m_CurrentSceneIndex;
-
-            evt.Fire();
-        }
     }
 }
